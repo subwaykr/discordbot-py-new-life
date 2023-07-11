10e006d3d30911960c50bf3e56ff0a9871f049ec
subwaykr <126319900+subwaykr@users.noreply.github.com>
2023-07-12 00:59:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,13 +30,13 @@ async def on_message(message):
     if message.content == f'{PREFIX}서버오픈' :
         msg = "안내봇 테스트중... @subway_kr"
         channel = client.get_channel('1098618958795526224')
-        await message.channel.send(msg)
+        await channel.send(msg)
 
 @client.event
 async def on_member_join(member) :
     msg = "안녕하세요! 뉴라이프에 오신것을 환영합니다!"
     channel = client.get_channel('1098618960297087050')
-    await message.channel.send(msg)
+    await channel.send(msg)
 
 try:
     client.run(TOKEN)
