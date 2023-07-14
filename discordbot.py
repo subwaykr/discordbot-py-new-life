0625c5fa7df7660c45c181d10ccf23eb2e1f991b
subwaykr <126319900+subwaykr@users.noreply.github.com>
2023-07-14 16:54:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,12 +3,9 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
-from discord.ext import commands
-from discord.utils import get
 intents = discord.Intents.all()
 intents.members = True
 load_dotenv()
-bot = commands.Bot
 
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
@@ -19,41 +16,36 @@ client = discord.Client()
 async def on_ready():
     print(f'Logged in as {client.user}.')
 
-@bot.command
-async def 서버오픈(ctx):
-    if ctx.message.author.guild_permissions.administrator:
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == f'{PREFIX}call':
+        await message.channel.send("callback!")
+
+    if message.content.startswith(f'{PREFIX}hello'):
+        await message.channel.send('Hello!')
+
+    if message.content == f'{PREFIX}서버오픈' :
         msg = "@everyone 서버가 오픈되었습니다! 서버를 즐겨보세요! 게임 접속 : https://www.roblox.com/games/13350091161/NewLife#!/game-instances"
         channel = client.get_channel(1098618958795526224)
         await channel.send(msg)
-    else :
-        await message.channel.send('관리자 전용 명령어 입니다.')
 
-@bot.command    
-async def 서버닫음(ctx) :
-    if ctx.message.author.guild_permissions.administrator:
+    if message.content == f'{PREFIX}서버닫음' :
         msg = "서버가 닫혔습니다! 다음 오픈을 기대해 주세요!"
         channel = client.get_channel(1098618958795526224)
         await channel.send(msg)
-    else :
-        await message.channel.send('관리자 전용 명령어 입니다.')
 
-@bot.command
-async def 점검실시(ctx) :
-    if ctx.message.author.guild_permissions.administrator:
+    if message.content == f'{PREFIX}점검실시' :
         msg = ":tools: 안내봇 점검 및 테스트중입니다! 이용에 착오없으시길 바랍니다."
         channel = client.get_channel(1098618958795526224)
         await channel.send(msg)
-    else :
-        await message.channel.send('관리자 전용 명령어 입니다.')
 
-@bot.command
-async def 점검완료(ctx) :
-    if ctx.message.author.guild_permissions.administrator:
+    if message.content == f'{PREFIX}점검완료' :
         msg = ":white_check_mark: 안내봇 점검이 종료되었습니다!"
         channel = client.get_channel(1098618958795526224)
         await channel.send(msg)
-    else :
-        await message.channel.send('관리자 전용 명령어 입니다.')
 
 @client.event
 async def on_member_join(member) :
